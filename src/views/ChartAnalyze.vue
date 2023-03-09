2be7d1be265d53fa986fc85807926b521cf6a181
駱筱尹 <eva090029@eri.com.tw>
2023-03-09 17:50:09 +0800

    feat: create pie chart, but still have showing problem

--- src/views/ChartAnalyze.vue
+++ src/views/ChartAnalyze.vue
@@ -1,37 +1,81 @@
 <script setup lang="ts">
 import { useSpendingStore } from '@/stores/useSpendingStore';
-import { onMounted, computed } from 'vue';
+import { onMounted, computed, ref } from 'vue';
+import type { ChartData, ChartOptions } from 'chart.js';
+import PieChart from '@/components/PieChart.vue';
 
 const spendingStore = useSpendingStore();
-onMounted(() => {
-  spendingStore.getExpense();
+
+// 取出 expense 後重組
+const calculate = computed(() => spendingStore.expenseList
+  .map((expense) => ({
+    date: expense.date,
+    amount: Number(expense.amount),
+    categoryName: expense.expenseCategory?.name,
+  }))
+  .filter((item) => {
+    console.log(new Date().toISOString().slice(0, 7));
+    return item.date.slice(0, 7) === new Date().toISOString().slice(0, 7);
+  }));
+
+/* eslint no-undef: "error" */
+const refactor = computed(() => calculate.value
+  .reduce((acc: Record<string, unknown>, item) => {
+    const { categoryName, amount } = item;
+    if (categoryName! in acc) {
+      acc[categoryName!] = (acc[categoryName!] as number) + amount;
+    } else {
+      acc[categoryName!] = amount;
+    }
+    return acc;
+  }, {}));
+
+// 圖表
+const expenseChartDate = ref<ChartData<'pie'>>({
+  labels: Object.keys(refactor.value),
+  // labels: ['飲食', '交通', '購物', '娛樂', 'undefined'],
+  datasets: [
+    {
+      label: '每月支出類別比例',
+      data: [100, 200, 300, 590, 100],
+      // data: Object.values(refactor.value),
+      backgroundColor: [
+        '#77CEFF',
+        '#0079AF',
+        '#123E6B',
+        '#97B0C4',
+        '#A5C8ED',
+      ],
+    },
+  ],
 });
 
-// const useExpenseCatogories = computed(() =>
-//  spendingStore.expenseList.filter((expense, index, arr) => arr.indexOf(expense.expenseCategory?.name) === index));
-const handleExpenseChart = computed(() => spendingStore.expenseList.map((expense) => expense.expenseCategory?.name));
+const chartOptions = ref<ChartOptions<'pie'>>({
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+    title: {
+      display: true,
+      text: 'title',
+    },
+  },
+});
 
-const expenseCategory = computed(() => handleExpenseChart.value.filter((category, index, arr) => arr.indexOf(category) === index));
-
-// const calculateExpense = computed(() => {
-//   spendingStore.expenseList.forEach((item) => {
-//     if (spendingStore.expenseList.expenseCategory?.name)
-//   })
-
-// })
-
-const test = computed(() => spendingStore.expenseList.map((expense) => `${expense.expenseCategory?.name}: ${expense.amount}`));
-
-onMounted(() => {
-  console.log(handleExpenseChart.value);
-  console.log(expenseCategory.value);
-  console.log(test.value);
-  // console.log(useExpenseCatogories.value);
+onMounted(async () => {
+  await spendingStore.getExpense();
+  expenseChartDate.value.labels = Object.keys(refactor.value);
+  console.log(calculate.value);
+  console.log(refactor.value);
+  console.log(Object.keys(refactor.value));
+  // console.log(Object.values(refactor.value));
 });
 </script>
 
 <template>
-  <div>
-    圖表分析
-  </div>
+
+  圖表分析
+  <PieChart
+    :expense-data="expenseChartDate"
+    :chartOptions="chartOptions"
+  />
 </template>
